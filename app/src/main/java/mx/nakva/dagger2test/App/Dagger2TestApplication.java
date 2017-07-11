7c2b71edcbab7eaba9269ff8b06b96b26b661953
Juancho <linanjm90@gmail.com>
2017-07-11 20:52:51 +0100

    add an app file class

---
+++ app/src/main/java/mx/nakva/dagger2test/App/Dagger2TestApplication.java
@@ -0,0 +1,17 @@
+package mx.nakva.dagger2test.App;
+
+import android.app.Application;
+
+/**
+ * Created by Juancho on 11/07/17.
+ * Nakva
+ * linanjm90@gmail.com
+ */
+
+public class Dagger2TestApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}

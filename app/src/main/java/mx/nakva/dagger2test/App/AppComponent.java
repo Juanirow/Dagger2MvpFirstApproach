9bd2d82a9bdb29715d74925e95e00d0600fca23f
Juancho <linanjm90@gmail.com>
2017-07-11 21:48:42 +0100

    add the presenter module to the app component

--- app/src/main/java/mx/nakva/dagger2test/App/AppComponent.java
+++ app/src/main/java/mx/nakva/dagger2test/App/AppComponent.java
@@ -3,6 +3,8 @@ package mx.nakva.dagger2test.App;
 import javax.inject.Singleton;
 
 import dagger.Component;
+import mx.nakva.dagger2test.Dagger.Modules.PresenterModule;
+import mx.nakva.dagger2test.Modules.Login.LoginActivity;
 
 /**
  * Created by Juancho on 11/07/17.
@@ -11,7 +13,9 @@ import dagger.Component;
  */
 
 @Singleton
-@Component(modules = {AppModule.class})
+@Component(modules = {AppModule.class, PresenterModule.class})
 public interface AppComponent {
+
+    void inject(LoginActivity target);
 
 }
